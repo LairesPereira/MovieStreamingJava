310a1027cda73ed5a2fce71b19cb5b6f899e3bd8
Laires <lairespsoares@gmail.com>
2024-05-28 12:56:09 -0300

    implent file name santization
    having movies with special chars dont impact over http request anymore

--- src/main/java/com/example/mycinema/models/PlayableFile.java
+++ src/main/java/com/example/mycinema/models/PlayableFile.java
@@ -4,6 +4,7 @@ import com.example.mycinema.Contracts.IPlayableInfo;
 import com.example.mycinema.Contracts.IPlayableTxtAditionalInfoFile;
 
 import java.io.File;
+import java.net.SocketTimeoutException;
 
 public abstract class PlayableFile implements IPlayableInfo, IPlayableTxtAditionalInfoFile {
     public String folderPath;
@@ -14,8 +15,8 @@ public abstract class PlayableFile implements IPlayableInfo, IPlayableTxtAdition
     public PlayableFile(String folderPath) {
         this.folderPath = folderPath;
         setPlayableFile(folderPath);
-        setFileName();
         setFileExtension();
+//        setFileName();
     }
 
     private void setPlayableFile(String folderPath) {
@@ -25,13 +26,28 @@ public abstract class PlayableFile implements IPlayableInfo, IPlayableTxtAdition
             if(file.toString().endsWith(".mp4") || file.toString().endsWith(".mkv")) {
                 this.file = file;
                 this.fileName = this.file.getName().trim().substring(0, file.getName().length() - 4);
+                this.extension = this.file.getName().trim().substring(file.getName().length() - 4);
+                String fileOriginalName = this.fileName;
+                String sanitizedName = fileOriginalName.replaceAll("[\\[\\]\\{\\}\\(\\)]","");
+                file.renameTo(new File(file.getParent() + "/" + sanitizedName + extension));
+                System.out.println(file);
+                System.out.println("Aqui");
+                System.out.println(sanitizedName);
+                System.out.println(this.extension);
+                System.out.println("Sanitized");
+                System.out.println(file.getParent() + "/" + sanitizedName + extension);
+                this.fileName = sanitizedName;
             }
         }
     }
 
     @Override
     public void setFileName() {
-        this.fileName = this.file.getName().trim().substring(0, file.getName().length() - 4);
+//        String fileOriginalName = this.file.getName().trim().substring(0, file.getName().length() - 4);
+//        String sanitizedName = fileOriginalName.replaceAll("[\\[\\]\\{\\}\\(\\)]","");
+//        System.out.println("Aqui");
+//        System.out.println(sanitizedName);
+//        this.fileName = sanitizedName;
     }
 
     @Override
