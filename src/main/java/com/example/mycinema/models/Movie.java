032e7a02c1a62f370ff93948660c002c8f461ff0
Laires <lairespsoares@gmail.com>
2024-05-27 23:30:54 -0300

    change image path to base64

--- src/main/java/com/example/mycinema/models/Movie.java
+++ src/main/java/com/example/mycinema/models/Movie.java
@@ -1,11 +1,19 @@
 package com.example.mycinema.models;
 
 import com.example.mycinema.Enums.EnumAgeGroup;
-
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Base64;
 import java.util.Scanner;
 
-public class Movie extends PlayableFile{
+public class Movie extends PlayableFile implements WebMvcConfigurer {
+
+    private final String PROVIDER_PATH = System.getProperty("user.dir");
+    private String SOURCE_PATH = PROVIDER_PATH.substring(0, PROVIDER_PATH.lastIndexOf("/")) + "/movies";
+
     public String gender;
     public String description;
     public String posterSource;
@@ -20,6 +28,13 @@ public class Movie extends PlayableFile{
         setAgeGroup();
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Ajuste o caminho conforme o local do seu diretório
+        registry.addResourceHandler(SOURCE_PATH + "/**")
+                .addResourceLocations(SOURCE_PATH);
+    }
+
     public void setMoviePoster() {
         File[] files = new File(super.folderPath).listFiles();
         for (File file : files) {
@@ -28,10 +43,28 @@ public class Movie extends PlayableFile{
                 File rename = new File(file.getParent() + "/" + fileName + extension);
                 file.renameTo(rename);
                 String posterParentPath = folderPath.substring(folderPath.lastIndexOf("/"));
-                this.posterSource = posterParentPath + "/" + file.getName();
+                File image = new File(folderPath + "/" + file.getName());
+                File f =  new File(image.getPath());
+                String encodstring = encodeFileToBase64Binary(f);
+                System.out.println(encodstring);
+                this.posterSource = "data:image/jpeg;base64," + encodstring;
+
                 break;
             }
         }
+    }
+
+    private static String encodeFileToBase64Binary(File file){
+        String encodedfile = null;
+        try {
+            FileInputStream fileInputStreamReader = new FileInputStream(file);
+            byte[] bytes = new byte[(int)file.length()];
+            fileInputStreamReader.read(bytes);
+            encodedfile = new String(Base64.getEncoder().encodeToString(bytes).getBytes(), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return encodedfile;
     }
 
     public void setMovieGender() {
